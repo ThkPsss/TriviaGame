c7d0c5a1ade3f70cec10de9e6097151b886ca6ee
ThkPsss <christanspence66@gmail.com>
2019-10-20 14:50:27 -0400

    Trying out a different method to figure out this problem.

---
+++ program.js
@@ -0,0 +1,50 @@
+/*<p>Which Champion is known as "The Bounty Hunter"?</p>
+                <div id="answers">
+                    <input type="checkbox">Graves</input> <input type="checkbox">Talon</input> <input type="checkbox">Miss Fortune</input> <input type="checkbox">Master Yi</input>
+                </div>
+
+
+
+
+
+                <p>Which champion says this line?: "Join the glorious evolution."</p>
+                <div id="answers">
+                    <input type="checkbox">Blitzcrank</input> <input type="checkbox">Orrn</input> <input type="checkbox">Viktor</input> <input type="checkbox">Olaf</input>
+                </div>
+
+
+
+
+                <p>Who was Gangplank's first love?</p>
+                <div id="answers">
+                    <input type="checkbox">Illaoi</input> <input type="checkbox">Lissandra</input> <input type="checkbox">Neeko</input> <input type="checkbox">Fiora</input>
+                </div>
+
+
+
+
+                <p>Which champion did Faker use in his famous outplay vs KT's Ryu in 2014?</p>
+                <div id="answers">
+                    <input type="checkbox">Zed</input> <input type="checkbox">Ryze</input> <input type="checkbox">Orianna</input> <input type="checkbox">LeBlanc</input>
+                </div>
+
+
+
+
+                
+                <p>What does Warwick's ult range scale off of?</p>
+                <div id="answers">
+                    <input type="checkbox">Attack Damage(AD)</input> <input type="checkbox">Ability Power(AP)</input> <input type="checkbox">Attack Speed(AS)</input> <input type="checkbox">Movement Speed(MS)</input>
+                </div>
+
+
+
+
+                <p>Which of the champions listed below is the communities most hated champion?</p>
+                <div id="answer">
+                    <input type="checkbox">Zed</input> <input type="checkbox">Riven</input> <input type="checkbox">Yasuo</input> <input type="checkbox">Pyke</input>
+                </div>*/
+
+
+
+                
